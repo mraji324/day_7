12712b05f3b7d8cb688c86470f363344b9b3ffa0
Hindu <rajuhindur@gmail.com>
2024-12-08 16:06:58 +0530

    Add files via upload

---
+++ Pattern5.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Pattern5
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i>1;++i)
+        {
+            for(int j=1;j>=1;++j)
+        {
+            System.out.print("*");
+        }
+        System.out.println();
+        }
+    }
+}
